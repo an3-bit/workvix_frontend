73d5715164b6d98af3d5a4a33dd16cb8060101cb
an3-bit <andrewmogambi12@gmail.com>
2025-05-23 22:26:09 +0300

    .

--- src/pages/home2/home2.tsx
+++ src/pages/home2/home2.tsx
@@ -125,7 +125,7 @@ const inspirationCategories = [
               Orders
             </Link>
             <Link to="/pro" className="text-sm font-medium hidden md:block">
-              Try workvix Pro
+              Try workvix Go
             </Link>
             
             {/* Icons */}
